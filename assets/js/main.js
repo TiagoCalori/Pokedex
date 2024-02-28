1a702572caec0391c8b6c65c0bf7828a86310b12
Tiago Calori <tiagofcalori@gmail.com>
2024-02-28 11:26:11 -0300

    Manipulando o resultado da requisição através de uma Promise

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,20 @@
 const offset = 0
 const limit = 10
 
-const url =  `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
-fetch(url).then(function(response){
-    console.log(response)
-})
- const x = 10 + 10
- console.log(x)
+fetch(url)
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (jsonBody) {
+        console.log(jsonBody)
+    })
+    // caso fracase a requisição .catch
+    .catch(function (error) {
+        console.error(error)
+    })
+    // independente o resultado da requesico se for com sucesso ou fracasso
+    .finally(function () {
+        console.log('Requisição concluída!')
+    })
